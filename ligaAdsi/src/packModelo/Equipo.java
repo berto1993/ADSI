a16a961107802b45485329bbaff79ae2fb419f23
Alberto <Alberto@56d8f3513859455.Home>
2014-01-11 18:58:13 +0100

    He empezado a crear las clases, solo atributos y constructora hasta que lleguen las correcciones de las pruebas nada más

---
+++ ligaAdsi/src/packModelo/Equipo.java
@@ -0,0 +1,26 @@
+package packModelo;
+
+public class Equipo 
+{
+	private String nombre;
+	private int puntos;
+	private ¿int? entrenador;
+	private String estadio;
+	private ListaJugadores jugadores;
+	private int idCod;
+	private ListaJugadores listaConvocados;
+	
+	public Equipo(String pNombre, String pEstadio, int pIdCodm ¿int? pEntrenador) 
+	{
+		this.nombre = pNombre;
+		this.estadio = pEstadio;
+		this.idCod = pIdCod;
+		puntos = 0;
+		entrenador = pEntrenador;
+		jugadores = null;
+		listaConvocados = null;
+	}
+	
+	
+
+}
--- ligaAdsi/src/packModelo/Jugador.java
+++ ligaAdsi/src/packModelo/Jugador.java
@@ -1,5 +1,26 @@
 package packModelo;
 
-public class Jugador {
+public class Jugador 
+{
+	private String nombre;
+	private int dorsal;
+	private int partidosJugados;
+	private int tarjetasRecibidas;
+	private boolean fichaje;
+	private boolean expulsion;
+	private String dni;
 
+	public Jugador (String pNombre,int pDorsal, String pDni)
+	{/*Por defecto al crear un jugador este no
+	habra jugado ningun partido ni tendra tarjeta,
+	ademas no estara disponible para fichar ni expulsado*/
+		
+		nombre = pNombre;
+		dorsal = pDorsal;
+		partidosJugados = 0;
+		tarjetasRecibidas = 0;
+		fichaje = false;
+		expulsion = false;
+		dni = pDni;
+	}
 }
