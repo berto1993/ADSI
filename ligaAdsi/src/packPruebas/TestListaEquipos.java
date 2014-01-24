6940d0f525834329910b56d8d0aa1a67bbb64e1f
Alberto <Alberto@56d8f3513859455.Home>
2014-01-24 16:11:45 +0100

    Junits calcular clasificacion echos

--- ligaAdsi/src/packPruebas/TestListaEquipos.java
+++ ligaAdsi/src/packPruebas/TestListaEquipos.java
@@ -45,21 +45,78 @@ public class TestListaEquipos {
 		
 		lista.ordenar();
 		Iterator<Equipo> it = lista.getIterator();
-		while (it.hasNext())
-		{
-			it.next().getPuntos();
-		}
-
+		
+		
+		aux =it.next();
+		assertEquals(aux.getPuntos(), 1);
+		aux =it.next();
+		assertEquals(aux.getPuntos(), 2);
+		aux =it.next();
+		assertEquals(aux.getPuntos(), 3);
+		aux =it.next();
+		assertEquals(aux.getPuntos(), 4);
+		aux =it.next();
+		assertEquals(aux.getPuntos(), 5);
+		
 	}
 
 	@Test
-	public void testGetEquipo() {
-		fail("Not yet implemented");
+	public void testGetEquipo() 
+	{
+		assertEquals(lista.tamano(),0);
+		Equipo aux = new Equipo("equi1", "campo1" ,1);
+		lista.add(aux);
+		aux = new Equipo("equi4", "campo4" ,4);
+		lista.add(aux);
+		aux = new Equipo("equi5", "campo1" ,5);
+		lista.add(aux);
+		aux = new Equipo("equi3", "campo3" ,3);
+		lista.add(aux);
+		aux = new Equipo("equi2", "campo2" ,2);
+		lista.add(aux);
+		assertEquals(lista.tamano(),5);
+	
+		aux = lista.getEquipo(4);
+		assertEquals(aux.getNombre(), "equi2s");
 	}
 
 	@Test
-	public void testGetEquipoN() {
-		fail("Not yet implemented");
+	public void testGetEquipoN() 
+	{
+		assertEquals(lista.tamano(),0);
+		Equipo aux = new Equipo("equi1", "campo1" ,1);
+		lista.add(aux);
+		aux = new Equipo("equi4", "campo4" ,4);
+		lista.add(aux);
+		aux = new Equipo("equi5", "campo1" ,5);
+		lista.add(aux);
+		aux = new Equipo("equi3", "campo3" ,3);
+		lista.add(aux);
+		aux = new Equipo("equi2", "campo2" ,2);
+		lista.add(aux);
+		assertEquals(lista.tamano(),5);
+		
+		aux = lista.getEquipoN("equi2");
+		assertEquals(aux.getPuntos(), 2);
+	}
+
+	public void  testGetEquipos() 
+	{
+		assertEquals(lista.tamano(),0);
+		Equipo aux = new Equipo("equi1", "campo1" ,1);
+		lista.add(aux);
+		aux = new Equipo("equi4", "campo4" ,4);
+		lista.add(aux);
+		aux = new Equipo("equi5", "campo1" ,5);
+		lista.add(aux);
+		aux = new Equipo("equi3", "campo3" ,3);
+		lista.add(aux);
+		aux = new Equipo("equi2", "campo2" ,2);
+		lista.add(aux);
+		assertEquals(lista.tamano(),5);	
+	
+		Equipo[] lis = lista.getEquipos();
+		assertEquals(lis.length, 5);
 	}
 
 }
